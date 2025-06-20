c1173dfc1a03b3978fc40483236489262a4758d6
yoyo <byr1361214482@outlook.com>
2025-06-20 07:48:36 +0000

    [automatic commit] 2025-06-20 07:48:36 UTC 101.174.8.137

--- part2/app.js
+++ part2/app.js
@@ -1,29 +1,19 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 require('dotenv').config();
 
-const userRoutes = require('./routes/userRoutes');
-const walkRoutes = require('./routes/walkRoutes');
-
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+// Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
 
-app.use(session({
-  secret: 'dog-walk-secret',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
 
-// Main routes
-app.use('/', userRoutes);
-app.use('/api', walkRoutes);
-
-// Start server
-app.listen(8080, () => {
-  console.log('Server running on http://localhost:8080');
-});
+// Export the app instead of listening here
+module.exports = app;
