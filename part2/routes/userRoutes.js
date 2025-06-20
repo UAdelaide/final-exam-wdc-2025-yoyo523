ab84a0ff76fa9ccee1cf8e80bfb492528d4bfe06
yoyo <byr1361214482@outlook.com>
2025-06-20 07:39:21 +0000

    [automatic commit] 2025-06-20 07:39:21 UTC 101.174.8.137

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.post('/login', async (req,res)=>{const { username, password } = req.body;
-try{const [rows]=await db}})
+try{const [rows]=await db.execute()}})
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
