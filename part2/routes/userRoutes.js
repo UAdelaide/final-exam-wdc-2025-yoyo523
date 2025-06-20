ea8cbd2be442d75cd3dbd1d6538e35217b7e45cf
yoyo <byr1361214482@outlook.com>
2025-06-20 08:20:08 +0000

    [automatic commit] 2025-06-20 08:20:08 UTC 101.174.8.137

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,10 +40,10 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const [rows] = await db.execute(
-      'SELECT * FROM users WHERE username = ? AND password_hash = ?',
-      [username, password]
-    );
+const [rows] = await db.execute(
+  'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
+  [username, password]
+);
 
     if (rows.length === 1) {
       req.session.user = rows[0];
