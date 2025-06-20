911ea189ce9e906bee7204d9a12ddc3d3ccbbd5b
yoyo <byr1361214482@outlook.com>
2025-06-20 11:04:31 +0000

    [automatic commit] 2025-06-20 11:04:31 UTC 101.174.8.137

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // allow form submissions
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
@@ -15,5 +22,15 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
+// Logout route
+app.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).send('Logout failed');
+    }
+    res.clearCookie('connect.sid');
+    res.redirect('/');
+  });
+});
+
 module.exports = app;
