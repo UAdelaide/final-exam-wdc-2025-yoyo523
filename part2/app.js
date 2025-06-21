df97bfce4a2a639c4a40b2d403b1bdd2b8f6547a
yoyo <byr1361214482@outlook.com>
2025-06-21 02:47:34 +0000

    [automatic commit] 2025-06-21 02:47:34 UTC 101.174.8.137

--- part2/app.js
+++ part2/app.js
@@ -37,6 +37,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+app.use('/api', require('./routes/walkRoutes'));
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
