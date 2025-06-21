e6a93655eebc76d58c0a37b1785cae743bfbd33a
yoyo <byr1361214482@outlook.com>
2025-06-21 02:30:50 +0000

    [automatic commit] 2025-06-21 02:30:50 UTC 101.174.8.137

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -62,9 +62,9 @@ router.post('/', async (req, res) => {
 
   try {
     const [result] = await db.query(`
-      INSERT INTO WalkRequests (dog_id, owner_id, requested_time, duration_minutes, location, status)
-      VALUES (?, ?, ?, ?, ?, 'open')
-    `, [dog_id, owner_id, requested_time, duration_minutes, location]);
+      INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+      VALUES (?, ?, ?, ?, 'open')
+    `, [dog_id, requested_time, duration_minutes, location]);
 
     res.status(201).json({ message: 'Walk request created', request_id: result.insertId });
   } catch (error) {
